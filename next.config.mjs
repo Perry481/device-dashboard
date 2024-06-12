4c55bf7f445fe332ea5d3db00d2e6394a5d85471
Perry481 <perry20990823@gmail.com>
2024-06-12 08:44:32 +0800

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      displayName: true,
+      fileName: false,
+    },
+  },
 };
 
 export default nextConfig;
