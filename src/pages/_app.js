d3530eaec76890bfb86c6b83105d21e175ad80e9
Perry481 <perry20990823@gmail.com>
2024-12-23 10:31:21 +0800

    update package, remove iframe and company context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,14 @@
-// src/pages/_app.js
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import TopNavigation from "../components/TopNavigation";
-import Footer from "../components/Footer";
-import IframeLayout from "../components/IframeLayout";
 import MainLayout from "../components/MainLayout";
 import { useRouter } from "next/router";
 import { CompanyProvider } from "../contexts/CompanyContext";
 import { LanguageProvider } from "../contexts/LanguageContext";
+
 const App = ({ Component, pageProps }) => {
   const [selectedPage, setSelectedPage] = useState("Index");
   const router = useRouter();
-  const isIframe = router.query.iframe === "true";
 
   const handlePageChange = (page) => {
     setSelectedPage(page);
@@ -47,60 +44,48 @@ const App = ({ Component, pageProps }) => {
   return (
     <LanguageProvider>
       <CompanyProvider>
-        {isIframe ? (
-          <IframeLayout>
-            <Component {...pageProps} />
-          </IframeLayout>
-        ) : (
-          <>
-            <Head>
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-              />
-              <title>設備儀錶板</title>
-              <style>{`
-              html, body, #__next {
-                height: 100%;
-              }
-              .wrapper {
-                min-height: 80%;
-                display: flex;
-                flex-direction: column;
-              }
-              .content-wrapper {
-                flex: 1 0 auto;
-                display: flex;
-                flex-direction: column;
-              }
-              .navbar-light .navbar-nav .active > .nav-link, 
-              .navbar-light .navbar-nav .nav-link.active, 
-              .navbar-light .navbar-nav .nav-link.show, 
-              .navbar-light .navbar-nav .show > .nav-link {
-                color: rgba(0, 0, 0, 0.9);
-                font-weight: bold;
-              }
-              .chart-container {
-                height: 400px;
-              }
-            `}</style>
-            </Head>
-            <div className="wrapper">
-              <TopNavigation
-                onPageChange={handlePageChange}
-                selectedPage={selectedPage}
-              />
-              <div className="content-wrapper">
-                <div className="container-fluid h-100">
-                  <MainLayout>
-                    <Component {...pageProps} isIframe={isIframe} />
-                  </MainLayout>
-                </div>
-              </div>
-              {/* <Footer /> */}
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>設備儀錶板</title>
+          <style>{`
+            html, body, #__next {
+              height: 100%;
+            }
+            .wrapper {
+              min-height: 80%;
+              display: flex;
+              flex-direction: column;
+            }
+            .content-wrapper {
+              flex: 1 0 auto;
+              display: flex;
+              flex-direction: column;
+            }
+            .navbar-light .navbar-nav .active > .nav-link, 
+            .navbar-light .navbar-nav .nav-link.active, 
+            .navbar-light .navbar-nav .nav-link.show, 
+            .navbar-light .navbar-nav .show > .nav-link {
+              color: rgba(0, 0, 0, 0.9);
+              font-weight: bold;
+            }
+            .chart-container {
+              height: 400px;
+            }
+          `}</style>
+        </Head>
+        <div className="wrapper">
+          <TopNavigation
+            onPageChange={handlePageChange}
+            selectedPage={selectedPage}
+          />
+          <div className="content-wrapper">
+            <div className="container-fluid h-100">
+              <MainLayout>
+                <Component {...pageProps} />
+              </MainLayout>
             </div>
-          </>
-        )}
+          </div>
+        </div>
       </CompanyProvider>
     </LanguageProvider>
   );
